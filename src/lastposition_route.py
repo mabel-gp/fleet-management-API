b05ba7d5328366bc27dad7ddde4e6237490dd0e6
mabel-gp <167148030+mabel-gp@users.noreply.github.com>
2024-10-27 22:31:27 -0500

    Ultima trayectoria de trajectories

--- src/lastposition_route.py
+++ src/lastposition_route.py
@@ -1,12 +1,27 @@
 """Last Position Endpoint"""
-from flask import Blueprint, request
+from flask import Blueprint, jsonify
+from sqlalchemy import desc
+from src.models import Trajectory
 
 lastPosition_blueprint = Blueprint('lastPosition_blueprint', __name__)
 
 @lastPosition_blueprint.route('/trajectories/latest', methods = ['GET'])
 def get_lastPosition():
 
-    #En construcción
-    page = request.args.get('page', default=1, type=int)
-    limit = request.args.get('limit', default=10, type=int)
+    #Consulta: último trayectoria de la tabla trajectories
+    database_query = Trajectory.query.order_by(
+        desc(Trajectory.date)
+        ).first()
 
+    dict_latest_trajectory = {
+        'taxiId': database_query.taxi_id,
+        'date': database_query.date,
+        'latitude': database_query.latitude,
+        'longitude': database_query.longitude,
+    }
+    return jsonify(dict_latest_trajectory), 200
+
+
+
+
+    

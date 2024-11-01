8ad51fbeaa5c19e35da188523f384e1046016219
mabel-gp <167148030+mabel-gp@users.noreply.github.com>
2024-10-31 23:41:29 -0500

    WIP: Mostrar última trajectory

--- src/lastposition_route.py
+++ src/lastposition_route.py
@@ -1,26 +1,51 @@
 """Last Position Endpoint"""
 from flask import Blueprint, jsonify
-from sqlalchemy import desc
-from src.models import Trajectory
+from sqlalchemy import select, func
+from src.models import Taxi, Trajectory, db
 
 lastPosition_blueprint = Blueprint('lastPosition_blueprint', __name__)
 
 @lastPosition_blueprint.route('/trajectories/latest', methods = ['GET'])
 def get_lastPosition():
 
-    #Consulta: último trayectoria de la tabla trajectories
-    database_query = Trajectory.query.order_by(
-        desc(Trajectory.date)
-        ).first()
+    session = db.session() #Interacción con la database
+    #Subconsulta 
+    sub_query = (
+        select(
+            Trajectory.taxi_id,
+            func.max(Trajectory.date).label('last_date')
+        )
+        .group_by(Trajectory.taxi_id)
+        .subquery()
+        )
 
-    dict_latest_trajectory = {
-        'taxiId': database_query.taxi_id,
-        'plate': database_query.taxi.plate,
-        'date': database_query.date,
-        'latitude': database_query.latitude,
-        'longitude': database_query.longitude,
-    }
-    return jsonify(dict_latest_trajectory), 200
+    # Consulta principal
+    main_query = (
+        select(
+            Trajectory.taxi_id,
+            Taxi.plate,
+            Trajectory.date,
+            Trajectory.latitude,
+            Trajectory.longitude
+        )
+        .join(sub_query, (Trajectory.taxi_id == sub_query.c.taxi_id)&(Trajectory.date == sub_query.c.last_date))
+        .join(Taxi, Trajectory.taxi_id == Taxi.id)
+        
+    )
+
+    last_trajectories = session.execute(main_query).all() # Ejecuta la consulta principal
+    #List, contiene el dic
+    last_trajc_dict = [
+        {
+        'taxiId': last_trajectory.taxi_id,
+        'plate': last_trajectory.plate,
+        'date': last_trajectory.date,
+        'latitude': last_trajectory.latitude,
+        'longitude': last_trajectory.longitude,
+        }
+        for last_trajectory in last_trajectories
+    ]
+    return jsonify(last_trajc_dict), 200
 
 
 

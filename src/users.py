0e10a719b249639f454f71b1bace8514571adf79
mabel-gp <167148030+mabel-gp@users.noreply.github.com>
2024-11-04 23:57:57 -0500

    WIP: GET - Agregar params: page y limit

--- src/users.py
+++ src/users.py
@@ -1,5 +1,5 @@
 """Users Endpoints"""
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from src.models import Users
 
 users_blueprint = Blueprint('users_blueprint', __name__)
@@ -7,6 +7,20 @@ users_blueprint = Blueprint('users_blueprint', __name__)
 @users_blueprint.route('/users', methods = ['GET'])
 def users():
 
-    database_query = Users.query.all()
+    page = request.args.get('page',default=1)
+    limit = request.args.get('limit', default=10)
+
+    # Values no válidos(x or y)
+    try:
+        page = int(page)
+        limit = int(limit)
+    except ValueError:
+        return jsonify({'error': 'Bad Request: Page or limit is not valid'}), 400
     
+    # Values no válidos(negativos)
+    if page <1 or limit <1:
+        return jsonify({'error': 'Bad Request: Page or limit is not valid'}), 400
+
+    database_query = Users.query.limit(limit).all()
+
     return jsonify([user.to_dictionary() for user in database_query]), 200
